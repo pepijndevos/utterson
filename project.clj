737a7de761a5e475c1decc5d226f45b2eb103775
Devin Walters <devinw@gmail.com>
2009-12-16 17:36:03 -0600

    scaffolded lein project -- you will need to recreated the classes test and lib directories yourself before running lein deps

---
+++ project.clj
@@ -0,0 +1,5 @@
+(defproject utterson "0.1"
+  :description "Utterson is a static site generator in the style of jekyll & hyde."
+  :dependencies [[org.clojure/clojure "1.1.0-alpha-SNAPSHOT"]
+                 [org.clojure/clojure-contrib "1.0-SNAPSHOT"]]
+  :dev-dependencies [[org.clojure/swank-clojure "1.0"]])
