bf5ff0d964200ec0ec808d637a89317ffcd1cb2a
Pepijn de Vos <pepijndevos@ubuntu.lan>
2009-12-23 16:44:22 +0100

    Fixed dependecies, it will built now!

--- project.clj
+++ project.clj
@@ -1,6 +1,7 @@
 (defproject utterson "0.1"
             :description "Utterson is a static site generator in the style of jekyll & hyde."
+            :repositories [["scala-tools" "http://scala-tools.org/repo-releases"]]
             :dependencies [[org.clojure/clojure "1.1.0-master-SNAPSHOT"]
                            [org.clojure/clojure-contrib "1.1.0-master-SNAPSHOT"]
-                           [com.petebevin.markdown "1.0.2"]]
+                           [org.markdownj/markdownj "0.3.0-1.0.2b4"]]
             :main utterson.generator)
