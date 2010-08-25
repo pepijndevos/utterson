8971ecb4201bbce8846072dd4ad422ffa4fac23b
Pepijn de Vos <pepijndevos@gmail.com>
2010-08-25 17:18:45 +0200

    clean slate

---
+++ README.markdown
@@ -0,0 +1,7 @@
+# Utterson #
+Yet another Jekyll and Hyde reference for a simple static site
+generator.  Only this time-- it's in Clojure!  Why?  Because!
+
+## Features ##
+
+ * to be determined
---
+++ project.clj
@@ -0,0 +1,8 @@
+(defproject utterson "0.1"
+            :description "Utterson is a static site generator in the style of jekyll & hyde."
+            :repositories [["scala-tools" "http://scala-tools.org/repo-releases"]]
+            :dependencies [[org.clojure/clojure "1.1.0-master-SNAPSHOT"]
+                           [org.clojure/clojure-contrib "1.1.0-master-SNAPSHOT"]
+                           [org.markdownj/markdownj "0.3.0-1.0.2b4"]
+                           [org.clojars.liebke/compojure "0.3.1"]]
+            :main utterson.generator)
