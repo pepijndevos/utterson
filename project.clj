914069c681d7433ce9bad19b2176a211d8e23440
Pepijn de Vos <pepijndevos@gmail.com>
2010-08-26 18:45:16 +0200

    modified project.clj

--- project.clj
+++ project.clj
@@ -1,8 +1,8 @@
 (defproject utterson "0.1"
             :description "Utterson is a static site generator in the style of jekyll & hyde."
-            :repositories [["scala-tools" "http://scala-tools.org/repo-releases"]]
-            :dependencies [[org.clojure/clojure "1.1.0-master-SNAPSHOT"]
-                           [org.clojure/clojure-contrib "1.1.0-master-SNAPSHOT"]
-                           [org.markdownj/markdownj "0.3.0-1.0.2b4"]
-                           [org.clojars.liebke/compojure "0.3.1"]]
+            :dependencies [[org.clojure/clojure "1.2.0"]
+                           [org.clojure/clojure-contrib "1.2.0"]
+                           [org.clojars.mcav/pegdown "0.8.5.1"]
+                           [enlive "1.0.0-SNAPSHOT"]]
+            :dev-dependencies [[org.clojars.brandonw/lein-nailgun "1.0.0"]]
             :main utterson.generator)
