61b37701529ce960a193edbee8541ae1c8e7b425
Pepijn de Vos <pepijndevos@gmail.com>
2010-09-23 13:19:02 +0200

    added counter

--- tasks.clj
+++ tasks.clj
@@ -6,9 +6,11 @@
 (deftask love
   "not war"
   (let [text (cycle ["unknown task: love"
+                     "You can't hurry love"
                      "Did you mean 'cake war'?\nAdd '--not=war' to ignore this message"
                      "We also love cake!"
-                     "Want a peace of cake?"])]
+                     "Want a peace of cake?"
+                     (str "Stop it! You've already wasted #" @index " cakes")])]
     (if (= ["war"] (:not *opts*))
       (println "Hippie!")
       (do
